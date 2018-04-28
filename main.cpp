e40c79f454eba317bb1fe5b206ff99610f4a24b1
Pedro Henrique Nardoni Ruiz <pedrohenriqueruiz@hotmail.com>
2018-04-28 00:41:14 -0300

    código mais desenvolvido
    Levar este código em conideração

--- main.cpp
+++ main.cpp
@@ -1,55 +1,74 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
+
 using namespace std;
 
 class Neuronio{
+	protected:
+	   int entradas;
+	   vector<double> w;
+	   double taxaAprendizagem ; //numero BEM pequeno
+	   
 	public:
-		virtual double funcAtivacao() = 0;
-		virtual double derivadaParcialEmW() = 0;
-		virtual ~Neuronio(){}
+	  Neuronio(int ent, double ta=0.0000000001){
+	  	entradas = ent;
+	  	taxaAprendizagem = ta;
+	  }
+	  
+		virtual double funcAtivacao(double x) = 0;
+		virtual double derivadaParcialEmW(vector<double> x, double y) = 0;
+		virtual double erro(vector<double> x);
+		virtual void itera(vector<double> x);
+		virtual ~Neuronio(){
+			
+		}
 	
 };
 
 
 class Perceptron1 : public Neuronio{
 	public:
-		double esperado(double x){
-			return 50*x*x; //input do neuronio, oq eu quero aprender
+	  Perceptron1(int entradas): Neuronio(entradas){
+	  	
+	  }
+	  
+		double esperado(vector<double> x){
+			return 50*x[0]*x[0]; //input do neuronio, oq eu quero aprender
 		}
 		
-		double funcAtivacao(double x){
-			return x*x; //funcao "dentro" do neuronio
+		double funcAtivacao(double xfa){
+			return xfa*xfa; //funcao "dentro" do neuronio
 		}
 		
-		double chute(double x){
-			return funcAtivacao(w1 * x); //funcao ativaçao com peso
+		double chute(vector<double> x){
+			return funcAtivacao(w[0] * x[0]); //funcao ativaçao com peso
 		}
 		
-		double derivadaParcialEmW(double y, double x){
+		double derivadaParcialEmW(vector<double> x, double y){
 			//2 w (w^2 x^4 - x^2 y); pra x^2 de ativaçao,
 		 //temos essa derivada, ela muda conforma a ativaçao escolhida
-			return 2*w1(w1*w1 * x*x*x*x - x*x * y);
+			return 2*w[0]*(w[0]*w[0] * x[0]*x[0]*x[0]*x[0] - x[0]*x[0] * y);
 		}
 		
-		double erro(double x){
+		double erro(vector<double> x){
 			double e = esperado(x) - chute(x);
 			e = e*e;
 			return e;
 		}
 		
-		void itera(double x){
-			w1 = w1 - derivadaParcialEmW(esperado(x), x)*taxaAprendizagem;
+		void itera(vector<double> x){
+			w[0] = w[0] - derivadaParcialEmW(x, esperado(x))*taxaAprendizagem;
 			cout<<erro(x)<<endl;
 		}
 		
-		
+		virtual ~Perceptron1(){}
 		
 	private://fazer setters e getters
-		double w1 = 0.8;
-		double taxaAprendizagem = 0.0000000001; //numero BEM pequeno
+		
 	
 };
-
+/*
 class Perceptron2 : public Neuronio{
 	public:
 		double esperado(double x){
@@ -101,14 +120,21 @@ class Perceptron3{
 	private:
 		double w3 = 
 		double taxaAprendizagem3 = 
-};
+};*/
 
 
 
 
 int main() {
 	
-	//Neuronio *n = new Perceptron1();
-	//delete n;
+	Neuronio *n = new Perceptron1(1);
+	vector<double> x(1);
+	
+	for(int i=0;i<10000;i++){
+		x[0] = i % 50;
+		n->itera(x);
+	}
+	
+	delete n;
   std::cout << "Hello World!\n";
 }
